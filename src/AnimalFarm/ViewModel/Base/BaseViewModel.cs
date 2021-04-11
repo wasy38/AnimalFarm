676155a803cead4753ea1df7e106f90a7feffddd
Pavel Okhotnikov <Alex5perevalov@mail.ru>
2021-04-11 18:49:54 +0700

    Add Base Class ViewModel

---
+++ src/AnimalFarm/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm.ViewModel.Base
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T filed, T value,[CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(filed, value)) return false;
+            filed = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
